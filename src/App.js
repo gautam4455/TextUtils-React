fc29488f64cd82e83d9e387f2dcbf9330118d7f3
gautam <gautam.kamble4455@gmail.com>
2022-01-16 21:17:55 +0530

    Navbar Added
    Navbar and heading added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Navbar from './components/Navbar';
+// import About from './components/About';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Clean react app</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<h1>TextUtil - React App</h1>
+		</div>
+	);
 }
 
 export default App;
